ae74586986427d7ee37fa086bac50b9584762c03
lfelipef1dev-jpg <lfelipef1.dev@gmail.com>
2026-08-05 22:34:07 -0300

    fix: use env vars

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,8 +6,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    'https://phhurravjunielzxatxe.supabase.co',
-    'sb_publishable_nuIOHxvxef55NYUKPV6FBQ_yH6pwfGc',
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -29,7 +29,7 @@ export async function createClient() {
 
 export function createServiceClient() {
   return createAdminClient(
-    'https://phhurravjunielzxatxe.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       auth: {
